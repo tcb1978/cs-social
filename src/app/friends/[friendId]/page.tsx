63ca530bda8c771513ef34d16fddc876d049ccbe
Matthew Eldredge <matthew.eldredge@photon.care>
2024-03-20 13:08:20 -0700

    includes dynamic routes according to postId, userId, friendId

---
+++ src/app/friends/[friendId]/page.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react';
+
+interface PageProps {
+  // props
+}
+
+const Page: FC<PageProps> = ({ }) => {
+  return (<div>FriendID</div>);
+};
+
+export default Page;
---
+++ src/app/users/[userId]/page.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react';
+
+interface PageProps {
+
+}
+
+const Page: FC<PageProps> = ({ }) => {
+  return (<div>userID</div>);
+};
+
+export default Page;
