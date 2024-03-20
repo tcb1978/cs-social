6b0e464bdd4e8f7806818948e2e62a558ac04e77
Matthew Eldredge <matthew.eldredge@photon.care>
2024-03-20 15:41:38 -0700

    setup jest with react-testing-library

---
+++ __tests__/app/dashboard/page.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Page from '../../../src/app/dashboard/page';
+import { getAuthenticatedUser } from '@/services/users/getAuthenticatedUser';
+
+jest.mock('@/services/users/getAuthenticatedUser');
+
+describe('Page Component', () => {
+  it('should render the AuthenticatedUserCard component when an authenticated user is present', async () => {
+    (getAuthenticatedUser as jest.Mock<Promise<any>>).mockResolvedValue({
+      username: 'testUser',
+      name: { first: 'Test', last: 'User' },
+    });
+
+    render(<Page />);
+
+    const authenticatedUserCard = await screen.findByText(/Welcome Test User/i);
+    expect(authenticatedUserCard).toBeInTheDocument();
+  });
+
+  it('should render a login prompt when no authenticated user is present', async () => {
+    (getAuthenticatedUser as jest.Mock<Promise<any>>).mockResolvedValue(null);
+
+    render(<Page />);
+
+    const loginPrompt = await screen.findByText(/Please login to continue/i);
+    expect(loginPrompt).toBeInTheDocument();
+  });
+
+  it('should render the Navigation component', () => {
+    (getAuthenticatedUser as jest.Mock<Promise<any>>).mockResolvedValue(null);
+
+    render(<Page />);
+
+    const navigationComponent = screen.getByText(/Landing/i);
+
+    expect(navigationComponent).toBeInTheDocument();
+  });
+});
