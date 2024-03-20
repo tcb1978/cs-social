55f0e7242f4765d5583d3863c54506e6750a020d
Matthew Eldredge <matthew.eldredge@photon.care>
2024-03-20 13:03:57 -0700

    includes additon of routes for dashboard, friends, posts, users

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react';
+
+interface PageProps {
+
+}
+
+const Page: FC<PageProps> = ({ }) => {
+  return (<div>POSTS</div>);
+};
+
+export default Page;
---
+++ src/app/users/page.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react';
+
+interface PageageProps {
+
+}
+
+const Pageage: FC<PageageProps> = ({ }) => {
+  return (<div>Pageage</div>);
+};
+
+export default Pageage;
